68d8b2a4a9b972d53f4339e274fa8310cd1243ed
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-04-08 12:29:03 -0300

    Context terminado

--- context/ui/UIContext.tsx
+++ context/ui/UIContext.tsx
@@ -2,6 +2,8 @@ import { createContext } from 'react';
 
 interface ContextProps {
   sideMenuOpen: boolean;
+  closeSideMenu: () => void;
+  openSideMenu: () => void;
 }
 
 export const UIContext = createContext({} as ContextProps);
--- context/ui/UIProvider.tsx
+++ context/ui/UIProvider.tsx
@@ -16,8 +16,12 @@ const UI_INITIAL_STATE: UIState = {
 export const UIProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
 
+  const openSideMenu = () => dispatch({ type: 'UI - Open Sidebar' });
+
+  const closeSideMenu = () => dispatch({ type: 'UI - Close Sidebar' });
+
   return (
-    <UIContext.Provider value={{ sideMenuOpen: false }}>
+    <UIContext.Provider value={{ ...state, closeSideMenu, openSideMenu }}>
       {children}
     </UIContext.Provider>
   );
