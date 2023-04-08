78686060d5ca0ff89a62e937eea7c5d2a212dc4c
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-04-07 22:47:48 -0300

    Sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from 'next';
 import { Typography } from '@mui/material';
+import Layout from '@/components/layouts/Layout';
 
 const HomePage: NextPage = () => {
   return (
-    <Typography variant='h1' color='primary'>
-      Hola Mundo
-    </Typography>
+    <Layout>
+      <Typography variant='h1' color='primary'>
+        Hola Mundo
+      </Typography>
+    </Layout>
   );
 };
 
