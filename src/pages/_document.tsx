2a876b17ee0b6586214d020b8f01d17b2c10ceb7
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-04-07 21:01:00 -0300

    Primer commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='es'>
       <Head />
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+      />
+      <link ref='shortcut icon' href='/favicon.ico' />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
